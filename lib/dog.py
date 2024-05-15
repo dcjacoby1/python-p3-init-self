8cc8d2e0c85b7a39d00120e38322bcb8314e75bc
Dan <jacobydan01@gmail.com>
2024-05-15 11:14:10 -0400

    init

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
+#init is called when an object is added to class
+#different then a def, bc def isn't performed on initialization of object
+#if no init is defined, python will automatically init behind the scences that does nothing
 class Dog:
-    pass
+    def __init__(self, name, breed = 'Mutt'):
+        self.name = name
+        self.breed = breed
